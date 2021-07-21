6df44464f5b0667d0f4a3160ed64fb57969e4b77
wacfeld wang <wacfeldwang333@gmail.com>
2021-07-21 06:00:28 -0400

    removed redundant things

--- main.c
+++ main.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
-//#include <assert.h>
-//#include <ctype.h>
-//#include <errno.h>
-//#include <float.h>
-//#include <limits.h>
-//#include <locale.h>
-//#include <math.h>
-//#include <setjmp.h>
-//#include <signal.h>
-//#include <stdarg.h>
-//#include <stddef.h>
-//#include <stdlib.h>
 #include <string.h>
-//#include <time.h>
 #define putd(x) printf(#x ": %d\n", x)
 #define MAXIN 100000
 #define maxrange sizeof(rangefreq)/sizeof(int)
